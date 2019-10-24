f8c393e9e9bc8207c5a99ab52ce3c41fa50c953a
jeffgrunewald <jeff@grunewalddesign.com>
2019-10-24 12:37:18 -0400

    Initial project setup

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
-# stargate
-An Apache Pulsar client written in Elixir
+# Stargate
+
+An Apache Pulsar client written in Elixir.
+
+## Installation
+
+If [available in Hex](https://hex.pm/docs/publish), the package can be installed
+by adding `stargate` to your list of dependencies in `mix.exs`:
+
+```elixir
+def deps do
+  [
+    {:stargate, "~> 0.1.0"}
+  ]
+end
+```
+
+Documentation can be generated with [ExDoc](https://github.com/elixir-lang/ex_doc)
+and published on [HexDocs](https://hexdocs.pm). Once published, the docs can
+be found at [https://hexdocs.pm/stargate](https://hexdocs.pm/stargate).
---
+++ mix.exs
@@ -0,0 +1,62 @@
+defmodule Stargate.MixProject do
+  use Mix.Project
+
+  def project() do
+    [
+      app: :stargate,
+      version: "0.1.0",
+      elixir: "~> 1.8",
+      start_permanent: Mix.env() == :prod,
+      deps: deps(),
+      docs: docs(),
+      package: package(),
+      description: description(),
+      source_url: "https://github.com/jeffgrunewald/stargate",
+      elixirc_paths: elixirc_paths(Mix.env()),
+      test_paths: test_paths(Mix.env())
+    ]
+  end
+
+  def application() do
+    [
+      extra_applications: [:logger]
+    ]
+  end
+
+  defp deps() do
+    [
+      {:credo, "~> 1.1", only: :dev, runtime: false},
+      {:divo, "~> 1.1.0", only: [:dev, :integration]},
+      {:divo_pulsar, "~> 0.1.0", only: [:dev, :integration]},
+      {:jason, "~> 1.1"},
+      {:mojito, "~> 0.5.0"},
+      {:websockex, "~> 0.4.0"},
+    ]
+  end
+
+  defp elixirc_paths(env) when env in [:test, :integration], do: ["lib", "test/support"]
+  defp elixirc_paths(_), do: ["lib"]
+
+  defp test_paths(:integration), do: ["test/integration"]
+  defp test_paths(_), do: ["test/unit"]
+
+  defp description(), do: "An Apache Pulsar client written in Elixir."
+
+  defp package() do
+    [
+      maintainers: ["jeffgrunewald"],
+      licenses: ["Apache 2.0"],
+      links: %{"GitHub" => "https://github.com/jeffgrunewald/stargate"}
+    ]
+  end
+
+  defp docs() do
+    [
+      main: "readme",
+      source_url: "https://github.com/jeffgrunewald/stargate",
+      extras: [
+        "README.md"
+      ]
+    ]
+  end
+end
