42bcb86100a7d9b5afd299db4ce3b4636c63d13c
jeffgrunewald <jeff@grunewalddesign.com>
2019-10-30 13:57:12 -0400

    config and ci setup

---
+++ config/integration.exs
@@ -0,0 +1,6 @@
+use Mix.Config
+
+config :stargate,
+  divo: [
+    {DivoPulsar, [port: 8080]}
+  ]
--- mix.exs
+++ mix.exs
@@ -26,6 +26,7 @@ defmodule Stargate.MixProject do
   defp deps() do
     [
       {:credo, "~> 1.1", only: :dev, runtime: false},
+      {:dialyxir, "~> 1.0.0-rc.7", only: [:dev], runtime: false},
       {:divo, "~> 1.1.0", only: [:dev, :integration]},
       {:divo_pulsar, "~> 0.1.0", only: [:dev, :integration]},
       {:jason, "~> 1.1"},
