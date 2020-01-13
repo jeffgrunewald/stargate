ba9582b1c2b0e81184cad9f2c8d4771a941d9a4f
jeffgrunewald <jeff@grunewalddesign.com>
2020-01-13 12:01:34 -0500

    testing cache restore keys

--- mix.exs
+++ mix.exs
@@ -14,7 +14,7 @@ defmodule Stargate.MixProject do
       source_url: "https://github.com/jeffgrunewald/stargate",
       elixirc_paths: elixirc_paths(Mix.env()),
       test_paths: test_paths(Mix.env()),
-      dialyzer: [plt_file: {:no_warn, ".dialyzer/#{System.version()}.plt"}]
+      dialyzer: [plt_file: {:no_warn, ".dialyzer/dialyzer-#{System.version()}.plt"}]
     ]
   end
 
